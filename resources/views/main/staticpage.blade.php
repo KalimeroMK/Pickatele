40b4f239aab6be8542ee6c6c0ac2a85ad613b24c
Martin Karadzinov <martin@myhost.mk>
2016-12-30 00:19:08 +0100

    merge

--- resources/views/main/staticpage.blade.php
+++ resources/views/main/staticpage.blade.php
@@ -42,32 +42,6 @@
                                     <img src="/assets/img/staticpage/{{$staticpage->image}}" alt="slidebg1"
                                          data-bgposition="center top" data-bgrepeat="no-repeat" data-bgfit="cover">
 
-                                    <!-- Transparent Background -->
-                                    <div class="tp-caption dark-translucent-bg"
-                                         data-x="center"
-                                         data-y="bottom"
-                                         data-speed="600"
-                                         data-start="0">
-                                    </div>
-
-                                    <!-- LAYER NR. 1 -->
-                                    <div class="tp-caption sfb fadeout text-center large_white"
-                                         data-x="center"
-                                         data-y="110"
-                                         data-speed="500"
-                                         data-start="1000"
-                                         data-easing="easeOutQuad">{{ $staticpage->title }}
-                                    </div>
-
-                                    <!-- LAYER NR. 2 -->
-                                    <div class="tp-caption sfb fadeout text-center large_white tp-resizeme"
-                                         data-x="center"
-                                         data-y="155"
-                                         data-speed="500"
-                                         data-start="1300"
-                                         data-easing="easeOutQuad">
-                                        <div class="separator light"></div>
-                                    </div>
 
                                     <!-- LAYER NR. 3 -->
                                     <div class="tp-caption sfb fadeout medium_white text-center"
