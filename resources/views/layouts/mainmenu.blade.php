ff3971572fc0739ab4c953c99ea575aeb3d312aa
heavyweight <kpapazov@gmail.com>
2017-01-04 18:06:19 +0100

    added images

--- resources/views/layouts/mainmenu.blade.php
+++ resources/views/layouts/mainmenu.blade.php
@@ -1,4 +1,4 @@
-@section('mainmenu')
+com@section('mainmenu')
     <!-- main-navigation start -->
     <!-- classes: -->
     <!-- "onclick": Makes the dropdowns open on click, this the default bootstrap behavior e.g. class="main-navigation onclick" -->
