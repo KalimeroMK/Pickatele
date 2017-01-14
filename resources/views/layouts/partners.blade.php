3c34abcf9f6f7ac0f04fac1107ddea5e2a08b644
heavyweight <kpapazov@gmail.com>
2017-01-14 23:26:46 +0100

    footer links order and links setup

--- resources/views/layouts/partners.blade.php
+++ resources/views/layouts/partners.blade.php
@@ -7,12 +7,61 @@
             <!-- ================ -->
             <div class="clients-container">
                 <div class="clients">
-                    @foreach($partners as $partner)
+                    <div class="client-image object-non-visible" data-animation-effect="fadeIn"
+                    data-effect-delay="100">
+                        <a href="https://www.tiggly.com/">
+                            <img src="/assets/images/partners/TigglyLogo.png" alt="TigglyLogo.png">
+                        </a>
+                    </div>
+
                     <div class="client-image object-non-visible" data-animation-effect="fadeIn"
                          data-effect-delay="100">
-                        <a href="#"><img src="/assets/img/partners/thumbnails/{{$partner->image}}" alt="{{ $partner->title }}"></a>
+                        <a href="http://www.kidstime.co.nz/">
+                            <img src="/assets/images/partners/kidstime_logo.png" alt="kidstime_logo.png">
+                        </a>
                     </div>
-                    @endforeach
+
+                    <div class="client-image object-non-visible" data-animation-effect="fadeIn"
+                         data-effect-delay="100">
+                        <a href="http://www.disney.com/">
+                            <img src="/assets/images/partners/disney.png" alt="disney.png">
+                        </a>
+                    </div>
+
+                    <div class="client-image object-non-visible" data-animation-effect="fadeIn"
+                         data-effect-delay="100">
+                        <a href="http://www.fingerprintplay.com/">
+                            <img src="/assets/images/partners/fingerprint.png" alt="fingerprint.png">
+                        </a>
+                    </div>
+
+                    <div class="client-image object-non-visible" data-animation-effect="fadeIn"
+                         data-effect-delay="100">
+                        <a href="https://www.phonicsschool.com/">
+                            <img src="/assets/images/partners/PhonicsSchool.png" alt="PhonicsSchool.png">
+                        </a>
+                    </div>
+
+                    <div class="client-image object-non-visible" data-animation-effect="fadeIn"
+                         data-effect-delay="100">
+                        <a href="http://kiddoz.se/">
+                            <img src="/assets/images/partners/kiddoz.png" alt="kiddoz.png">
+                        </a>
+                    </div>
+
+                    <div class="client-image object-non-visible" data-animation-effect="fadeIn"
+                         data-effect-delay="100">
+                        <a href="http://kiddoz.se/">
+                            <img src="/assets/images/partners/TrueColor.png" alt="TrueColor.png">
+                        </a>
+                    </div>
+
+                    {{--@foreach($partners as $partner)--}}
+                    {{--<div class="client-image object-non-visible" data-animation-effect="fadeIn"--}}
+                         {{--data-effect-delay="100">--}}
+                        {{--<a href="#"><img src="/assets/img/partners/thumbnails/{{$partner->image}}" alt="{{ $partner->title }}"></a>--}}
+                    {{--</div>--}}
+                    {{--@endforeach--}}
 
                 </div>
             </div>
