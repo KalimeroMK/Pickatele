b76a208ec53bb0c0baf7aecd10f67c2ff2cb9b39
heavyweight <kpapazov@gmail.com>
2017-01-15 01:06:16 +0100

    books style changes

--- resources/views/main/book.blade.php
+++ resources/views/main/book.blade.php
@@ -14,6 +14,7 @@
             <div class="col-xs-12 col-sm-4 pull-left">
                 <img src="/assets/img/books/thumbnails/{{ $book->image }}"/>
 
+                @if (count($bookimages))
                 <div class="col-xs-12 book-category-slider" style="margin-left:-25px">
                     <a href="#" style="left:-10px" class="prev inside-prev"><img src="/assets/images/left-arrow.png" /></a>
                     <div class="inside-carousel owl-carousel">
@@ -25,6 +26,7 @@
                     </div>
                     <a href="#" style="right:-15px" class="next inside-next"><img src="/assets/images/right-arrow.png" /></a>
                 </div>
+                @endif
             </div>
 
 
@@ -38,19 +40,33 @@
                     <span class="emphased-text">Genre</span>
                     <span class="genre">{{ $book->genre->name }}</span>
 
+                    @if ($book->video)
                     <span class="emphased-text" style="margin-left:50px;margin-right:10px">Book Trailer</span>
-                    <a href="{{ $book->video }}"><img style="display: inline-block"
-                                                      src="/assets/images/book-details/trailer.png"/></a>
+                    <a href="{{ $book->video }}">
+                        <img style="display: inline-block" src="/assets/images/book-details/trailer.png"/>
+                    </a>
+                    @endif
                 </p>
 
                 <p style="margin-top:50px;margin-bottom:15px" class="emphased-text">Enjoy this and 1000+ other
                     interactive stories</p>
 
                 <ul style="margin-top:0" class="store-links">
-                    <li style="padding-left:0"><a href="#"><img
-                                    src="/assets/images/book-details/1-apple-store.png"/></a></li>
-                    <li><a href="#"><img src="/assets/images/book-details/2-google-store.png"/></a></li>
-                    <li><a href="#"><img src="/assets/images/book-details/3-cn-store.png"/></a></li>
+                    <li style="padding-left:0">
+                        <a href="https://itunes.apple.com/us/app/pickatale-200+-interactive/id669433655?mt=8">
+                            <img src="/assets/images/book-details/1-apple-store.png"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://play.google.com/store/apps/details?id=com.WisdomEdition.Pickatale.Bookshelf&hl=en">
+                            <img src="/assets/images/book-details/2-google-store.png"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://www.wandoujia.com/apps/com.quduedu.pickatale">
+                            <img src="/assets/images/book-details/3-cn-store.png"/>
+                        </a>
+                    </li>
                 </ul>
 
             </div>
