e0413c3df88d61a0955aa7c38a3d2d3bcbc1ef38
heavyweight <kpapazov@gmail.com>
2017-01-15 15:48:08 +0100

    careers accordion changes

--- resources/views/main/careers.blade.php
+++ resources/views/main/careers.blade.php
@@ -42,60 +42,60 @@
             <div class="row">
                 <h1 class="col-xs-12">
                     <h1 class="section-header text-center">Join our team, make a difference and grow with us</h1>
+
                     <div class="text" style="color:#7ccaea;font-weight:600;margin: 50px 0">
-                    <p>Pickatale is an ed-tech startup based out of China and Macedonia. Our core business is the
-                        digital publishing of interactive children books for young ages. We currently have over 1000
-                        books and a planned production in the years to come.</p>
+                        <p>Pickatale is an ed-tech startup based out of China and Macedonia. Our core business is the
+                            digital publishing of interactive children books for young ages. We currently have over 1000
+                            books and a planned production in the years to come.</p>
 
-                    <p>We are on a mission to create the best, most useful and most interesting digital library of
-                        storybooks for children. If you want to help transform the world of reading for children, you'll
-                        love it here.
-                        Pickatale is pushing boundaries in technology and education, and is currently building an
-                        adaptive learning platform to accelerate children's learning.</p>
+                        <p>We are on a mission to create the best, most useful and most interesting digital library of
+                            storybooks for children. If you want to help transform the world of reading for children,
+                            you'll
+                            love it here.
+                            Pickatale is pushing boundaries in technology and education, and is currently building an
+                            adaptive learning platform to accelerate children's learning.</p>
 
-                    <p>We work with some of the best companies in the industry, including Disney, BabyFirst and Phonics
-                        School.
-                        Pickatale is always looking for motivated and talented individuals to join our family and help
-                        us test the boundaries of education technology.</p>
+                        <p>We work with some of the best companies in the industry, including Disney, BabyFirst and
+                            Phonics
+                            School.
+                            Pickatale is always looking for motivated and talented individuals to join our family and
+                            help
+                            us test the boundaries of education technology.</p>
                     </div>
                     <h1 class="section-header text-center">Explore career opportunities</h1>
+
                     <p>&nbsp;</p>
-                    <div class="accordion" id="accordion">
+
+                    <div id="accordion-links">
                         <div class="accordion-group">
                             <div class="accordion-heading">
-                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
-                                   href="#collapse1">
-                                    Frontend developer
-                                </a>
-                            </div>
-
-                            <div id="collapse1" class="accordion-body collapse">
-                                <div class="accordion-inner">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                                    deserunt mollit anim id est laborum.
+                                <div class="col-xs-12">
+                                    <div class="col-xs-8">
+                                        <a class="accordion-link pull-left" href="#developer">
+                                            Frontend developer
+                                        </a>
+                                    </div>
+                                    <div class="col-xs-4">
+                                        <a class="accordion-link pull-right" href="#developer">
+                                            Open
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div class="accordion-group">
                             <div class="accordion-heading">
-                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion"
-                                   href="#collapse2">
-                                    Designer
-                                </a>
-                            </div>
-
-                            <div id="collapse2" class="accordion-body collapse">
-                                <div class="accordion-inner">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                                    deserunt mollit anim id est laborum.
+                                <div class="col-xs-12">
+                                    <div class="col-xs-8">
+                                        <a class="accordion-link pull-left" href="#designer">
+                                            Designer
+                                        </a>
+                                    </div>
+                                    <div class="col-xs-4">
+                                        <a class="accordion-link pull-right" href="#designer">
+                                            Open
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
