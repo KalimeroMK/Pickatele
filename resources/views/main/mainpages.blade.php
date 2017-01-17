93af80e5b7e2bb467ea125b788447e9d4e8cf19a
Martin Karadzinov <martin@myhost.mk>
2017-01-17 22:58:02 +0100

    How it works title - mobile helper push class

--- resources/views/main/mainpages.blade.php
+++ resources/views/main/mainpages.blade.php
@@ -92,7 +92,7 @@
                 <div class="clearfix"></div>
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-12 col-xs-12">
+                        <div class="col-md-12 col-xs-12 mobile-push">
                             <h2 class="section-header text-center red">{!! $mainpage->secondheadertitle !!}</h2>
 
                         </div>
